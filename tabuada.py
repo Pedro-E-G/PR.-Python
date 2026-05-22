201bf67c92555ca3283c314d7663d9da5765f709
Pedro-E-G <pedro.paulino.garcia@escola.pr.gov.br>
2026-05-22 15:07:43 -0300

    Update tabuada.py

--- tabuada.py
+++ tabuada.py
@@ -1,1 +1,8 @@
+# Tabuada Simples
 
+numero = int(input("Digite um número para ver a tabuada: "))
+
+print(f"\nTabuada do {numero}:")
+
+for i in range(11):
+    print(f"{numero} x {i} = {numero * i}")
